f1689f471c4d60f8ec085f79609789deb0edcd79
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2022-11-14 18:08:16 +0200

    update challenge tests

--- helpers/challengesTests.js
+++ helpers/challengesTests.js
@@ -1,3 +1,5 @@
+// params, for stings, put them inside single quote, then double quote '""'
 module.exports = {
-  "dog-years": { id: 1, params: 1_000_000_000, expectedResult: "221.82" },
+  "dog-years": { id: 1, params: ['"venus"', 189839836], expectedResult: "68.45" },
+  "ispowerof2": { id: 2, params: [512], expectedResult: "false" },
 }
