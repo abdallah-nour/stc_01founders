a8f866df24f736f4af406305fa5419c17c1a39fa
jddev <hsd.salaheddine@gmail.com>
2022-11-15 15:26:00 +0000

    change tests

--- helpers/challengesTests.js
+++ helpers/challengesTests.js
@@ -1,5 +1,9 @@
 // params, for stings, put them inside single quote, then double quote '""'
 module.exports = {
-  "dog-years": { id: 1, params: ['"venus"', 189839836], expectedResult: "68.45" },
-  "ispowerof2": { id: 2, params: [512], expectedResult: "false" },
-}
+    "dog-years": {
+        id: 1,
+        params: ['"venus"', 189839836],
+        expectedResult: "68.45",
+    },
+    ispowerof2: { id: 2, params: [513], expectedResult: "false" },
+};
