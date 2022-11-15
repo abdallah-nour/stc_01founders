a0d9516d6ed19a92890d203945536d35b499cfc6
jddev <hsd.salaheddine@gmail.com>
2022-11-15 16:48:40 +0000

    add more challenges

--- helpers/challengesTests.js
+++ helpers/challengesTests.js
@@ -1,10 +1,11 @@
 // params, for stings, put them inside single quote, then double quote '""'
 module.exports = {
+    "sum-birds": { id: 1, params: [[1, 2, 3]], expectedResult: "6" },
+    "hello-world": { id: 2, params: [], expectedResult: "Hello, World!" },
     "dog-years": {
-        id: 1,
+        id: 3,
         params: ['"venus"', 189839836],
         expectedResult: "68.45",
     },
-    ispowerof2: { id: 2, params: [513], expectedResult: "false" },
-    "Hello-world": { id: 3, params: [], expectedResult: "Hello, World!" },
+    ispowerof2: { id: 4, params: [513], expectedResult: "false" },
 };
--- routes/challenge.js
+++ routes/challenge.js
@@ -11,7 +11,7 @@ const { isAuthenticated, checkUser } = require("../middleware/auth");
 
 // Route path
 router.get("/challenges", isAuthenticated, checkUser, getChallenges);
-router.post("/create_challenge", createChallenge);
+router.post("/create_challenge", isAuthenticated, checkUser, createChallenge);
 router.post("/delete_challenge", isAuthenticated, checkUser, deleteChallenge);
 router.get("/challenge/:title", isAuthenticated, checkUser, displayChallenge);
 
