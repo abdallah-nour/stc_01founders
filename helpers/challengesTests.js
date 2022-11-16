59bfce418678e7193d95d7b3230ff180d6c4e784
jddev <hsd.salaheddine@gmail.com>
2022-11-16 09:51:52 +0000

    change challenge tests

--- helpers/challengesTests.js
+++ helpers/challengesTests.js
@@ -1,7 +1,7 @@
 // params, for stings, put them inside single quote, then double quote '""'
 module.exports = {
     "sum-birds": { id: 1, params: [[1, 2, 3]], expectedResult: "6" },
-    "hello-world": { id: 2, params: [], expectedResult: "Hello, World!" },
+    "hello-world": { id: 2, params: ["str"], expectedResult: "Hello, World!" },
     "dog-years": {
         id: 3,
         params: ['"venus"', 189839836],
