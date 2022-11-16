0e2ad4a4058e423c82347300f39ac2f05825932a
jddev <hsd.salaheddine@gmail.com>
2022-11-16 11:00:02 +0000

    change users route

--- routes/users.js
+++ routes/users.js
@@ -28,15 +28,5 @@ router.get("/logout", logoutUser);
 
 router.get("/users", isAuthenticated, usersList);
 
-router.post("/run", async (req, res) => {
-    const { lang = "js", code } = req.body;
-
-    if (code === undefined)
-        return res.status(401).json({ Error: "Empty code body" });
-    const filePath = await generateFile(lang, code);
-    const output = await executeCode(filePath);
-    return res.json({ filePath: filePath, output: output });
-});
-
 // Export module
 module.exports = router;
