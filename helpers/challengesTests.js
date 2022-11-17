7cd6b2fda96cda1c80084192713656925bd34ed6
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2022-11-18 01:02:12 +0200

    update challenges tests

--- helpers/challengesTests.js
+++ helpers/challengesTests.js
@@ -1,11 +1,11 @@
 // params, for stings, put them inside single quote, then double quote '""'
 module.exports = {
-    "sum-birds": { id: 1, params: ["[1, 2, 3]"], expectedResult: "6" },
-    "hello-world": { id: 2, params: [""], expectedResult: "Hello, World!" },
-    "dog-years": {
-        id: 3,
-        params: ['"venus"', 189839836],
-        expectedResult: "68.45",
-    },
-    ispowerof2: { id: 4, params: [513], expectedResult: "false" },
+  "sum-birds": { id: 1, params: ["[1, 2, 3]"], expectedResult: "6" },
+  "hello-world": { id: 2, params: [""], expectedResult: "Hello, World!" },
+  "dog-years": {
+    id: 3,
+    params: ['"venus"', 189839836],
+    expectedResult: "68.45",
+  },
+  ispowerof2: { id: 4, params: [1, 513, 16], expectedResult: [true, false, true] },
 };
